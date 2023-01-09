2df4f6d7d55f4a79c9c651b05231e2d5ad5063d2
GeoSauer <geoffrey.sauer89@gmail.com>
2023-01-09 16:18:25 -0700

    add Animal component roughed out and rendering via .map in Main component

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,11 @@
-export default function Animal() {
-  return <></>;
+import { animals } from '../../data';
+import './Animal.css';
+
+export default function Animal(props) {
+  return (
+    <div className="animal" style={{ top: props.top, left: props.left }}>
+      <span className="name">{props.name}</span>
+      <span>{props.says}</span>
+    </div>
+  );
 }
