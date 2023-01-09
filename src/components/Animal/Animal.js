4ed5afba02348d3858ab90cea088738c3acaa108
GeoSauer <geoffrey.sauer89@gmail.com>
2023-01-09 16:26:55 -0700

    add image to Animal component

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -4,6 +4,7 @@ import './Animal.css';
 export default function Animal(props) {
   return (
     <div className="animal" style={{ top: props.top, left: props.left }}>
+      <img alt={props.name} src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
       <span className="name">{props.name}</span>
       <span>{props.says}</span>
     </div>
